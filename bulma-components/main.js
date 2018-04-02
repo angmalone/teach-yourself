148409377bb69f57318fabf3b0a3040600cbdc6c
Angela Malone <a.malone1193@gmail.com>
2018-04-02 13:36:18 -0400

    deleting with directives inline

--- bulma-components/main.js
+++ bulma-components/main.js
@@ -6,23 +6,18 @@ Vue.component("message", {
       isVisible: true
     };
   },
+  //v-show means it will show if it is true
   template: `<article class="message" v-show="isVisible" >
         <div class="message-header">
             {{ title }}
-            <button type="button" @click="hideModal">x</button>
+            <button type="button" @click="isVisible = false">x</button>
         </div>
 
         <div class="message-body">
             {{ body }}
         </div>
     <article>
-    `,
-
-  methods: {
-    hideModal() {
-      this.isVisible = false;
-    }
-  }
+    `
 });
 
 new Vue({
