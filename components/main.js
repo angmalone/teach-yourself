256298ac87f3423711831f5100adaf690b79b9e9
Angela Malone <a.malone1193@gmail.com>
2018-04-02 12:37:55 -0400

    component in a component practice

--- components/main.js
+++ components/main.js
@@ -1,9 +1,26 @@
+Vue.component("task-list", {
+  //whenever there is a template in a component it needs to have a single root element,
+  //there's not a single root since we're using v-for since there are siblings.
+  //to account for this, wrap the template in a div
+  template: `<div>
+            <task v-for="task in tasks"> {{ task.task }} </task>
+        </div>`,
+
+  data() {
+    return {
+      tasks: [
+        { task: "Go to class", complete: true },
+        { task: "Learn Vue", complete: true },
+        { task: "Learn Ruby", complete: false }
+      ]
+    };
+  }
+});
+
 Vue.component("task", {
   //slot acts as a placeholder for whatever the user inputs
   template: "<li><slot></slot></li>"
 });
-
-//YOU HAVE TO SET DATA EQUAL TO A FUNCTION THAT RETURNS AN OBJECT
 
 new Vue({
   el: "#root"
