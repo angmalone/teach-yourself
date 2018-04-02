5b61b1bd47796611b14d1666316f4c857718d5d1
Angela Malone <a.malone1193@gmail.com>
2018-04-02 13:34:30 -0400

    deleting w/methods

--- bulma-components/main.js
+++ bulma-components/main.js
@@ -1,15 +1,28 @@
 Vue.component("message", {
   props: ["title", "body"],
-  template: `<article class="message">
+
+  data() {
+    return {
+      isVisible: true
+    };
+  },
+  template: `<article class="message" v-show="isVisible" >
         <div class="message-header">
             {{ title }}
+            <button type="button" @click="hideModal">x</button>
         </div>
 
         <div class="message-body">
             {{ body }}
         </div>
     <article>
-    `
+    `,
+
+  methods: {
+    hideModal() {
+      this.isVisible = false;
+    }
+  }
 });
 
 new Vue({
